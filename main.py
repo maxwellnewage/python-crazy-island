ce959bd5ec7868a7a1b252533b88dc1fbfce6337
Maximiliano Burgos <maxib2007@gmail.com>
2023-07-13 16:08:17 -0300

    inclui las comillas triples, pero tuve que usar dedent por el indentado

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from entities.bat import Bat
 from controls.menu import show_menu
+from textwrap import dedent
 
 if __name__ == '__main__':
     bat = Bat()
 
-    event = f"apareció un {bat} salvaje!\n{bat.draw()}\nQue deseas hacer?"
+    event = f"""
+    apareció un {bat} salvaje!
+    {bat.draw()}
+    Que deseas hacer?
+    """
+
+    event = dedent(event)
 
     options = ["Mirarlo fijamente", "Correr", "Atacar", "Llorar"]
     show_menu(event, options)
